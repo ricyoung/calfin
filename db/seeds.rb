6e63c557befed14d6123ee5f86e68c6947180157
ric <ricyoung@gmail.com>
2015-05-09 02:08:12 +0000

    First and Best Commit

---
+++ db/seeds.rb
@@ -0,0 +1,35 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# create three Events
+3.times do |i|
+  Event.create(
+      name: "Event #{i + 1}", 
+      :start => DateTime.now,
+      location: "Location #{i + 1}")
+end
+
+# create five Users
+5.times do |i|
+    User.create(
+        name: "User #{i + 1}",
+        email: "user#{i + 1}@email.com",
+    )
+end
+
+# associate Events and Users as follows
+# event_id | user_id
+# ----------------------
+#     1          1
+#     1          2
+Event.find(1).users << User.find(1)
+Event.find(1).users << User.find(2)
+Event.find(2).users << User.find(3)
+Event.find(2).users << User.find(4)
+Event.find(3).users << User.find(5)
